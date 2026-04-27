3eea79e16a7dd8cf97db2b81c7448ede2b1530e2
JuanBaldenegro248272 <juan.rascon248272@potros.itson.edu.mx>
2026-04-27 06:12:24 -0700

    Merge delete
    Se borraro nunas cosas con el merge, se agregaron

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Dashboard from './pages/Dashboard';
 import Accesos from './pages/Accesos';
+import Login from './pages/Login/Login';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="accesos" element={<Accesos />} />
+          <Route path="ventas" element={<div>Página de Ventas</div>} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
